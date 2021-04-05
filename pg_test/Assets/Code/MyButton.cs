34d3ad282f03479654808fc49c1fbd682c9e2cad
RinR1 <72516282+RinR1@users.noreply.github.com>
2021-04-05 17:39:12 +0900

    21/04/05

---
+++ pg_test/Assets/Code/MyButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyButton : MonoBehaviour
+{
+
+    public string szDispName;
+    public string szFileName;
+    public byte byType = 0;
+
+    public void SetInfo(string szName, string szFile, byte byValue)
+    {
+        szDispName = szName;
+        szFileName = szFile;
+        byType = byValue;
+
+        transform.Find("Text").GetComponent<Text>().text = szDispName;
+    }
+
+    public void OnClickMyButton()
+    {
+        Debug.Log("Button Click => " + szDispName);
+    }
+}
